d1b48f035464c44557e6101f0a0ddb8cddc5c2b3
Gichia <petergichia35@gmail.com>
2022-04-22 08:38:22 +0300

    add new way of creating and filling forms

--- static/js/demo/datatables-demo.js
+++ static/js/demo/datatables-demo.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
   $('#dataTable').DataTable();
   $('#dataTable2').DataTable();
 
-  // $('#custom-accordion-one').on('click', function (e) {
-  //   console.log("Hello")
-  //   $('.collapse').collapse();
-  // })
+  $('.custom-accordion').on('click', function (e) {
+    if ($(".collapse").hasClass("show")) {
+      $('.collapse').removeClass("show");;
+    };
+  })
 });
